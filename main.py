7980b3ee167db3ffa9e0b74a8018c2aee6430c80
KELECHIDAVIS <kboss16058@gmail.com>
2023-02-04 21:22:55 -0800

    moved file in

---
+++ main.py
@@ -0,0 +1,37 @@
+import tensorflow as tf 
+
+#load mnist 
+
+mnist = tf.keras.datasets.mnist
+
+(xTrain , yTrain) , (xTest, yTest) = mnist.load_data(); 
+
+xTrain , xText = xTrain / 255.0 , xTest / 255.0
+
+
+#model 
+model =tf.keras.models.Sequential([
+    tf.keras.layers.Flatten(input_shape =(28,28)),
+    tf.keras.layers.Dense(128, activation ='relu'),
+    tf.keras.layers.Dropout(.2),
+    tf.keras.layers.Dense(10)
+])
+
+predictions = model(xTrain[:1]).numpy()
+
+print(tf.nn.softmax(predictions).numpy())
+
+lossFN = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+
+print(lossFN (yTrain[:1], predictions).numpy())
+
+
+model.compile(optimizer ='adam',
+              loss= lossFN,
+              metrics= ['accuracy'])
+
+#train model 
+model.fit(xTrain, yTrain, epochs = 5)
+
+model.evaluate(xTest, yTest , verbose=2)
+
